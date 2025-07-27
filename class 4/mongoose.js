3bff8b045bf35f6fde2bc9cf9a8b9b2feaa4c386
Areeba-binteali <areeba42101@gmail.com>
2025-07-27 16:01:00 +0500

    Updated mongoose.js

--- class 4/mongoose.js
+++ class 4/mongoose.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt")
 const Product = require("./models/product");
+const User = require("./models/user");
 const MONGODB_URI = "mongodb://localhost:27017/ecomStore";
 
 const app = express();
@@ -14,7 +16,7 @@ mongoose.connect(MONGODB_URI)
   .catch(err => console.log(err))
     
 
-app.get("/product", async (req, res) => {
+app.get("/products", async (req, res) => {
   try {
     const productModel = await Product.find();
     res.send({
@@ -25,6 +27,51 @@ app.get("/product", async (req, res) => {
     console.log(err)
   }
 });
+
+// Posting using schema
+// app.post("/products", async (req, res) => {
+//   const newProduct = new Product(req.body);
+//   const allProducts = await Product.find();
+//   allProducts.push(newProduct);
+//   res.send({
+//     message: "Succesfully Added Product!",
+//     product: allProducts,
+//   })
+
+// });
+
+// users
+app.get("/api/users", async (req, res) => {
+  try {
+    const user = await User.find();
+    res.status(200).send({
+      message: "users data",
+      data: user,
+    })
+  } catch (error) {
+    res.status(500).send({
+      message: "Error in getting Data",
+      error: error,
+    })
+  }
+});
+
+// posting new user
+app.post("/api/users", async (req, res) => {
+  try {
+    const newUser = req.body;
+    const password = await bcrypt.hash(newUser.password, 10)
+    const user = new User({ ...newUser, password: password });
+    await user.save();
+    res.send({
+      data: user,
+      message: "success",
+    })
+  } catch (error) {
+    res.status(500).send({data: null, success: false, error})
+  }
+});
+
 
 // Server
 const port = 3337;
